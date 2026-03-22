6286f31ddcca119dc36e11caac73bc312db2f70a
meebox <meebox@gmail.com>
2026-03-22 23:40:16 +0800

    加上自動重新連線的範例

--- 07_resume.py
+++ 07_resume.py
@@ -8,16 +8,13 @@ load_dotenv()
 
 client = genai.Client()
 
-# handle = None
-handle="CihybTViNHprYndpeDdheGs3czZpaWZyb2lmaW14YXV1M252bjBtdHo2"
-
 MODEL = "gemini-2.5-flash-native-audio-preview-12-2025"
 CONFIG = {
     "response_modalities": ["AUDIO"],
     "system_instruction": "使用繁體中文回答。",
     "output_audio_transcription": {}, # 取得生成語音的文字
     "session_resumption" :{
-        "handle": handle
+        "handle": None
     }
 }
 
@@ -27,7 +24,6 @@ async def input_loop(live_session: genai.live.AsyncSession):
         await live_session.send_realtime_input(text=prompt)
 
 async def message_loop(live_session: genai.live.AsyncSession):
-    global handle
     while True:
         async for message in live_session.receive():
             if message.go_away is not None:
@@ -37,7 +33,7 @@ async def message_loop(live_session: genai.live.AsyncSession):
                 update = message.session_resumption_update
                 if update.resumable and update.new_handle:
                     print(f"交談階段識別碼：{update.new_handle}")
-                    handle = update.new_handle
+                    CONFIG["session_resumption"]["handle"] = update.new_handle
                 continue
 
             content = message.server_content
@@ -58,25 +54,24 @@ async def message_loop(live_session: genai.live.AsyncSession):
                 print("\n\n> ", end="", flush=True)
 
 async def main():
-    try:
-        async with client.aio.live.connect(
-            model=MODEL, config=CONFIG
-        ) as live_session:
-            print("已連線。\n> ", end="", flush=True)
-            async with asyncio.TaskGroup() as tg:
-                tg.create_task(message_loop(live_session))
-                tg.create_task(input_loop(live_session))
-    except asyncio.CancelledError:
-        pass
-    finally:
-        print("\n\n程式結束")
-
-if __name__ == "__main__":
     while True:
         try:
-            asyncio.run(main())
-        # except genai.errors.APIError as e:
-        except Exception as e:
-            print(f"程式錯誤：{e}")
+            async with client.aio.live.connect(
+                model=MODEL, config=CONFIG
+            ) as live_session:
+                print("已連線。\n> ", end="", flush=True)
+                async with asyncio.TaskGroup() as tg:
+                    tg.create_task(message_loop(live_session))
+                    tg.create_task(input_loop(live_session))
+        except ExceptionGroup as e:
+            for e in e.exceptions:
+                print(f"強制斷線：{e.message}")
             continue
+        except asyncio.CancelledError:
+            pass
+        finally:
+            print("\n\n程式結束")
         break
+
+if __name__ == "__main__":
+    asyncio.run(main())
